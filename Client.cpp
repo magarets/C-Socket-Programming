0bb9a8a858fe02f8f965467943269bb820cdaa5c
yoonan <101767824+magarets@users.noreply.github.com>
2022-08-17 23:05:25 +0900

    update
    update

---
+++ Client.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32")
+
+#define PORT 33481
+#define PACKET_SIZE 1024
+#define SERVER_IP "10.1.1.130"
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+
+int main() {
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET hSocket;
+	hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	inet_pton(AF_INET, SERVER_IP, &(tAddr.sin_addr));
+
+	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+	char cMsg[] = "Seong Yoo won";
+	send(hSocket, cMsg, strlen(cMsg), 0);
+
+	char cBuffer[PACKET_SIZE] = {};
+	recv(hSocket, cBuffer, PACKET_SIZE, 0);
+
+	printf("Recv Msg: %s\n", cBuffer);
+
+	closesocket
+	(hSocket);
+
+	WSACleanup();
+}
